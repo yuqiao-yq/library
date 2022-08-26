53a2e88f870d898d5367ae1df6c2956f4c181655
yq <1207427994@qq.com>
2022-08-27 00:37:12 +0800

    build

--- src/components/frontend/Button/Btn3/index.tsx
+++ src/components/frontend/Button/Btn3/index.tsx
@@ -4,6 +4,7 @@ import style from './index.less';
 export default function index() {
   return (
     <div className={style.container}>
+      <h3>边框按钮</h3>
       <button className={style.btn}>边框按钮</button>
     </div>
   );
