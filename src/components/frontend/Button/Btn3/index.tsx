9f5135bd41a25aae1d4f4ead6cb0eb9bcdddc43f
yq <1207427994@qq.com>
2022-08-27 08:25:19 +0800

    button样式调整

--- src/components/frontend/Button/Btn3/index.tsx
+++ src/components/frontend/Button/Btn3/index.tsx
@@ -5,7 +5,9 @@ export default function index() {
   return (
     <div className={style.container}>
       <h3>边框按钮</h3>
-      <button className={style.btn}>边框按钮</button>
+      <div className={style.Btn3}>
+        <button className={style.btn}>边框按钮</button>
+      </div>
     </div>
   );
 }
