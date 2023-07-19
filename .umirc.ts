c3b69316df1e28a29543a22e5728be495444019d
yuqiao <1207427994@qq.com>
2023-07-19 18:45:14 +0800

    c++ first commit

--- .umirc.ts
+++ .umirc.ts
@@ -56,6 +56,7 @@ export default defineConfig({
       title: '后端',
       path: '/backend',
       children: [
+        { title: 'C++', path: '/backend/c' },
         { title: 'SQL', path: '/backend/sql' },
         { title: 'Java', path: '/backend/Java' },
         { title: 'Python', path: '/backend/Python' },
