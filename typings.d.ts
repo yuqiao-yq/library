c3657906bcba6114726b3d55e9283e1a88fff518
yq <1207427994@qq.com>
2024-09-10 17:13:54 +0800

    build

--- typings.d.ts
+++ typings.d.ts
@@ -1,2 +1,11 @@
 declare module '*.css';
-declare module '*.less';
+// declare module '*.less';
+declare module '*.less' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.module.less' {
+  const content: { [className: string]: string };
+  export default content;
+}
